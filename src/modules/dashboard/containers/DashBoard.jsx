9b3c7b7600f459f69698eff02cac3ce2c5b351cb
rungtruong <41542332+rungtruong@users.noreply.github.com>
2020-03-15 21:24:09 +0700

    test custom react select

--- src/modules/dashboard/containers/DashBoard.jsx
+++ src/modules/dashboard/containers/DashBoard.jsx
@@ -1,115 +1,80 @@
 import React, { Component } from "react";
-import Select, { components } from "react-select";
+import Select from "react-select";
+import "react-select-plus/dist/react-select-plus.css";
+import { css } from "emotion";
 
-import { css } from "@emotion/core";
 export default class DashBoard extends Component {
   state = {
     selectedOption: ""
   };
   handleChange = selectedOption => {
     this.setState({ selectedOption });
-    console.log(`Selected: ${selectedOption.label}`);
+    console.log(selectedOption);
   };
   render() {
     const { selectedOption } = this.state;
-    const value = selectedOption && selectedOption.value;
+    const value = selectedOption.value;
     const options = [
       {
-        label: "Group1",
-        type: "group",
-        options: [
-          { label: "Option 1", value: "1", type: "option" },
-          {
-            type: "group",
-            label: "Option 2",
-            value: "2",
-            options: [
-              { label: "Option 2-a", value: "2a", type: "option" },
-              { label: "Option 2-b", value: "2b", type: "option" }
-            ]
-          }
-        ]
+        label: "Primary ",
+        value: "1",
+        level: "0"
       },
       {
-        label: "Group2",
-        type: "group",
-        options: [
-          { label: "Option 3", value: "3", type: "option" },
-          {
-            type: "group",
-            label: "Option 4",
-            value: "2",
-            options: [
-              { label: "Option 4-a", value: "4a", type: "option" },
-              { label: "Option 4-b", value: "4b", type: "option" }
-            ]
-          }
-        ]
+        label: "Primary-1.1",
+        value: "1-1",
+        level: "1"
+      },
+      {
+        label: "Primary-1.1.1",
+        value: "1-1-1",
+        level: "2"
+      },
+      {
+        label: "Secondary",
+        value: "2"
+      },
+      {
+        label: "Secondary-2.1",
+        value: "2-1",
+        level: "1"
+      },
+      {
+        label: "Secondary2.2",
+        value: "2-2",
+        level: "1"
       }
     ];
-
-    const renderNestedOption = (props, label, nestedOptions) => {
-      const { cx, getStyles, innerProps, selectOption } = props;
-      // Will be applied to nested optgroup headers
-      const nestedLabelClassName ="nested-optgroup-label";
-      //  cx(
-      //  css(getStyles("groupHeading", props)),
-      //   { option: true },
-      //   "nested-optgroup-label"
-      // );
-      return (
-        <div className='nested-optgroup'>
-          <div className={nestedLabelClassName}>{label}</div>
-          {nestedOptions.map(nestedOption => {
-            if (nestedOption.options) {
-              return renderNestedOption(
-                props,
-                nestedOption.label,
-                nestedOption.options
-              );
-            }
-            const nestedInnerProps = innerProps;
-            nestedInnerProps.onClick = () => {
-              console.log(nestedOption);
-              
-              selectOption(nestedOption);
-            };
-            return (
-              <div className='nested-optgroup-option' key={nestedOption.value}>
-                <components.Option {...props} innerProps={nestedInnerProps}>
-                  {nestedOption.label}
-                </components.Option>
-              </div>
-            );
-          })}
-        </div>
-      );
-    };
-
-    const OptionCustom = props => {
-      const { children, data } = props;
-      const nestedOptions = data.options;
-
-      if (nestedOptions) {
-        // console.log(nestedOptions);
-
-        const label = data.label;
-        return renderNestedOption(props, label, nestedOptions);
+    const customStyles = {
+      // control: styles => ({ ...styles, backgroundColor: "white" }),
+      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+        // if (data.level)
+          return {
+            ...styles,
+            paddingLeft: (data.level*10+10)
+          };
+        // else if (data.level === "2")
+        //   return {
+        //     ...styles,
+        //     paddingLeft: 30
+        //   };
+        // else
+        //   return {
+        //     ...styles,
+        //     paddingLeft: 10
+        //   };
       }
-
-      return <components.Option {...props}>{children}</components.Option>;
     };
-
     return (
-      <div className='content'>
+     <div className="content">
         <Select
-          menuIsOpen
-          // value={value}
-          onChange={this.handleChange}
-          options={options}
-          components={{ Option: OptionCustom }}
-        />
-      </div>
+        // menuIsOpen
+        // value={value}
+        onChange={this.handleChange}
+        options={options}
+        styles={customStyles}
+      />
+     </div>
     );
   }
 }
